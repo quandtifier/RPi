8750a5c7f181a8fe03a43be546fd3baad628a205
quandtifier <quandtifier@gmail.com>
2018-04-25 15:05:05 -0700

    intensity base on current_wind now but avg still broken

--- sc1.py
+++ sc1.py
@@ -9,12 +9,14 @@
 
 # Program Spec
 # sc1.py opens a connection to MySQLdb using a existing
-# user and database.  It then reads the connected sound monitor
+# user and database.  It then reads the connected sound sensor
 # and potentiometer every second and commits that data to the 
 # specified database with an abrieviated timestamp.
+# The database is queried based on several criteria 
+# then an LED simulating noise cancellation is adjusted accordingly
 
 # Program setup requirements
-# - Connect the sound monitor to port A1
+# - Connect the sound sensor to port A1
 # - Connect the potentiometer to port A2
 # - Connect the LED to port D4
 
@@ -24,13 +26,15 @@
 # - Observe the raw readings produce from the sound monitor (not DBs)
 # - End program using the ctrl+c
 
-
+from decimal import Decimal
 
 # Get the necessary libraries for interfacing
 import MySQLdb
 import grovepi
 import time
 import datetime
+
+
 
 #connect to MySQLdb
 #                    <host>      <MySQL user>   <pwrd>  <db_name>
@@ -39,8 +43,8 @@ db=MySQLdb.connect("localhost", "sc1program", "tcss499", "sc1")
 # enable traversal of the relation db
 cursor=db.cursor(); 
 
-# Connect the sound monitor to analog port A1
-soundmonitor = 1
+# Connect the sound sensor to analog port A1
+soundsensor = 1
 # Connect the potentiometer to analog port A2
 potentiometer = 2
 
@@ -57,42 +61,66 @@ while True:
         	# get the system time
 		localtime = datetime.datetime.now()
 		
-		# store the current soundmonitor reading
-		current_noise = grovepi.analogRead(soundmonitor)
+		# store the current sound sensor reading
+		current_noise = grovepi.analogRead(soundsensor)
 		# store the current 'wind speed'
-		current_wind = grovepi.analogRead(potentiometer)
+		current_wind = grovepi.analogRead(potentiometer) / 10
 
 		# print data, localtime is parsed to have form: <'HH:MM:SS'>
-		print('Time {}  ::  Reading {}'.format(localtime.strftime('%H:%M:%S'), current_noise))
-		print('Time {}  ::  Reading {}'.format(localtime.strftime('%H:%M:%S'), current_wind))
+		print('Noise -> Time {}  ::  Reading {}'.format(localtime.strftime('%H:%M:%S'), current_noise))
+		print('Wind -> Time {}  ::  Reading {}'.format(localtime.strftime('%H:%M:%S'), current_wind))
         	
+
         	# create the MySQL INSERT INTO statement
 		insert_noise = (
             		"INSERT INTO micreads (rtime, reading) "
             		"VALUES (%s, %s)"
         	)
-
+		data_noise = (localtime.strftime('%H:%M:%S'), current_noise)
+		
 		# create the MySQL INSERT INTO statement
 		insert_wind = (
             		"INSERT INTO windspeeds (rtime, reading) "
             		"VALUES (%s, %s)"
         	)
-		
-		# store the data which corresponds to the '%s' in VALUES clause
-        	data_noise = (localtime.strftime('%H:%M:%S'), current_noise)
 		data_wind = (localtime.strftime('%H:%M:%S'), current_wind)
+        	
+
 		
 		# save the current readings to the database
         	try:
             		cursor.execute(insert_noise, data_noise)
 			cursor.execute(insert_wind, data_wind)
             		db.commit()
+			
         	except:
             		db.rollback()
 		
 		# stall to limit one reading each second
 		time.sleep(1)
-	
+
+		# get read to query the database for the last 5 second windspeed average
+		tminusten = localtime - datetime.timedelta(seconds=5)
+		tminusten = tminusten.strftime('%H:%M:%S')
+		average_wind_query = (
+			"SELECT AVG(windspeeds.reading) as avgs "
+			"FROM windspeeds "
+			"WHERE windspeeds.rtime > %s"
+		)
+		data_avg_wind = (tminusten)
+		avg_wind_delta_5 = 0
+		cursor.execute(average_wind_query, data_avg_wind)
+		for (avgs) in cursor:
+			print('avg wind: {}'.format(avgs))
+			avg_wind_delta_5 = avgs
+
+		output_intensity = 0
+		if current_wind < 75.0:
+			output_intensity = current_noise
+			print('Out: {}'.format(output_intensity))
+
+		print('avg: {}'.format(avg_wind_delta_5))
+
 	# error logging
 	except IOError:
 		print("Error")
